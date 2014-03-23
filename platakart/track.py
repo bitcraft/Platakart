12e291ee58bcf8f76a7b076b4569b23cd2f2ef7c
William Kevin Manire <williamkmanire@gmail.com>
2014-03-22 21:36:25 -0700

    Final steps for kart model

--- platakart/track.py
+++ platakart/track.py
@@ -25,6 +25,9 @@ from platakart.ui import WHITE
 PERCENT_COLOR = WHITE
 FADE_COLOR = BLACK
 
+from collections import namedtuple
+Kart = namedtuple("Kart", "chassis, rear_motor, front_motor")
+
 
 class TrackScene(Scene):
 
@@ -63,44 +66,57 @@ class TrackScene(Scene):
 
     def make_kart(self, space):
         # set up chassis
-        mass = 100
+        mass = 10
         chassis_body = pymunk.Body(mass, pymunk.inf)
         chassis_body.position = 150, 300
-        chassis = pymunk.Segment(chassis_body, (-75, 0), (75, 0), 5.0)
+        chassis = pymunk.Segment(chassis_body, (-75, 0), (75, 0), 15.0)
 
         # set up wheels
         mass = 1
         radius = 14
         inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
         damping = 0.0
-        stiffness = 250.0
+        stiffness = 300.0
 
         rear_wheel_body = pymunk.Body(mass, inertia)
+        rear_wheel_body.friction = 100
         rear_wheel_body.position = (100, 250)
         rear_wheel = pymunk.Circle(rear_wheel_body, radius, (0, 0))
         rear_wheel_body_spring = pymunk.DampedSpring(
-            rear_wheel_body, chassis_body, (0, 0), (-50, 0), 40.0,
+            rear_wheel_body, chassis_body, (0, -15), (-50, -15), 50.0,
             stiffness, damping)
 
         front_wheel_body = pymunk.Body(mass, inertia)
+        front_wheel_body.friction = 100
         front_wheel_body.position = (200, 250)
         front_wheel = pymunk.Circle(front_wheel_body, radius, (0, 0))
         front_wheel_body_spring = pymunk.DampedSpring(
-            front_wheel_body, chassis_body, (0, 0), (50, 0), 40.0,
+            front_wheel_body, chassis_body, (0, -15), (50, -15), 50.0,
             stiffness, damping)
 
-        space.add(chassis_body, chassis, rear_wheel_body, rear_wheel,
-                  rear_wheel_body_spring,
-                  front_wheel_body, front_wheel,
-                  front_wheel_body_spring)
+        rear_slide_joint = pymunk.SlideJoint(chassis_body, rear_wheel_body,
+                                             (-50, -15), (0, 0), 27.0, 50.0)
+        front_slide_joint = pymunk.SlideJoint(chassis_body, front_wheel_body,
+                                              (50, -15), (0, 0), 27.0, 50.0)
 
-        return chassis
+        motor1 = pymunk.SimpleMotor(chassis_body, front_wheel_body, 0.0)
+        motor2 = pymunk.SimpleMotor(chassis_body, rear_wheel_body, 0.0)
+
+        space.add(chassis_body, chassis, rear_wheel_body, rear_wheel,
+                  rear_wheel_body_spring, rear_slide_joint,
+                  front_wheel_body, front_wheel,
+                  front_wheel_body_spring, front_slide_joint,
+                  motor1, motor2)
+
+        return Kart(chassis_body, motor2, motor1)
 
     def on_key_up(self, key, mod):
-        if key == K_RIGHT:
-            pass
-        elif key == K_LEFT:
-            pass
-        elif key == K_UP:
-            for kart in self.karts:
-                kart.body.apply_impulse((0, 32000))
+        for kart in self.karts:
+            if key == K_RIGHT:
+                kart.rear_motor.rate = -100.0
+                kart.front_motor.rate = -100.0
+            elif key == K_LEFT:
+                kart.rear_motor.rate = 100.0
+                kart.front_motor.rate = 100.0
+            elif key == K_UP:
+                    kart.chassis.apply_impulse((0, 6000))

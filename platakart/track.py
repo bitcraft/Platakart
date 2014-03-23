eb78d05f989963cb349e5fdb9134313b66d059db
William Kevin Manire <williamkmanire@gmail.com>
2014-03-22 20:12:12 -0700

    Hilarious disaster

--- platakart/track.py
+++ platakart/track.py
@@ -31,11 +31,9 @@ class TrackScene(Scene):
     def __init__(self, resources, conf):
         self.resources = resources
         self.space = None
+        self.karts = None
         self.step_amt = 1.0 / float(conf.get("target_fps", 30))
         logger.debug("Calculated space step amount = %f" % self.step_amt)
-        self.balls = None
-        self.lines = None
-        self.contacts = None
 
     def get_name(self):
         return "track"
@@ -43,14 +41,14 @@ class TrackScene(Scene):
     def setup(self):
         logger.debug("Setting up track scene")
         self.space = pymunk.Space()
-        self.space.add_collision_handler(0, 0, None, None,
-                                         self.add_collision, None)
-        self.contacts = list()
         self.space.gravity = (0.0, -900.0)
-        self.balls = list()
-        for i in range(1):
-            self.balls.append(self.add_ball())
-        self.lines = self.add_static_L(self.space)
+
+        # floor
+        floor = pymunk.Segment(pymunk.Body(), (0, 10), (640, 10), 5.0)
+        self.space.add(floor)
+
+        self.karts = list()
+        self.karts.append(self.make_kart(self.space))
         pub.subscribe(self.on_key_up, "input.key-up")
 
     def teardown(self):
@@ -63,57 +61,46 @@ class TrackScene(Scene):
         pymunk.pygame_util.draw(screen, self.space)
         pygame.display.flip()
 
-    def make_wheel(self):
-        mass = 10
-        radius = 14
-        inertia = pymunk.moment_for_circle(mass, 0, radius)
-        body = pymunk.Body(mass, inertia)
-        x = random.randint(120, 380)
-        body.position = x, 550
-        body.ang_vel_limit = 2
-        shape = pymunk.Circle(body, radius)
-        self.space.add(body, shape)
-        return shape
+    def make_kart(self, space):
+        # set up chassis
+        mass = 100
+        moment = pymunk.moment_for_segment(mass, (-50, 0), (50, 0))
+        chassis_body = pymunk.Body(mass, moment)
+        chassis_body.position = 150, 150
+        chassis = pymunk.Segment(chassis_body, (-50, 0), (50, 0), 5.0)
 
-    def make_kart(self):
-        pass
-
-    def add_ball(self):
+        # set up rear-suspension
         mass = 1
-        radius = 14
-        inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
-        body = pymunk.Body(mass, inertia)
-        x = random.randint(120, 380)
-        body.position = x, 200
-        body.friction = 0.5
-        shape = pymunk.Circle(body, radius)
-        self.space.add(body, shape)
-        return shape
+        moment = pymunk.moment_for_segment(mass, (0, 0), (0, 10))
+        rear_suspension_body = pymunk.Body(mass, moment)
+        rear_suspension_body.position = 100, 160
+        rear_suspension = pymunk.Segment(
+            rear_suspension_body, (0, 0), (0, 25), 1.0)
+        rear_suspension_body_joint = pymunk.SlideJoint(
+            rear_suspension_body, chassis_body, (0, 0), (-50, 0), 10.0, 15.0)
 
-    def add_collision(self, space, arb):
-        self.contacts = arb.contacts
+        # set up front-suspension
+        mass = 1
+        moment = pymunk.moment_for_segment(mass, (0, 0), (0, 10))
+        front_suspension_body = pymunk.Body(mass, moment)
+        front_suspension_body.position = 100, 160
+        front_suspension = pymunk.Segment(
+            front_suspension_body, (0, 0), (0, 25), 1.0)
+        front_suspension_body_joint = pymunk.SlideJoint(
+            front_suspension_body, chassis_body, (0, 0), (50, 0), 10.0, 15.0)
 
-    def add_static_L(self, space):
-        body = pymunk.Body()
-        body.position = (0, 10)
-        body.friction = 0.5
-        l1 = pymunk.Segment(body, (0, 0), (640, 0), 5)
-        l2 = pymunk.Segment(body, (10, 0), (10, 480), 5)
-        l3 = pymunk.Segment(body, (630, 0), (630, 480), 5)
+        space.add(chassis_body, chassis, 
+                  rear_suspension_body, rear_suspension, rear_suspension_body_joint,
+                  front_suspension_body, front_suspension, front_suspension_body_joint)
 
-        self.space.add(l1, l2, l3)
-        return [l1, l2, l3]
+
+        return chassis
 
     def on_key_up(self, key, mod):
         if key == K_RIGHT:
-            for ball in self.balls:
-                body = ball.body
-                body.apply_impulse((100, 0), (-10, 10))
+            pass
         elif key == K_LEFT:
-            for ball in self.balls:
-                body = ball.body
-                body.apply_impulse((-100, 0), (-10, 10))
+            pass
         elif key == K_UP:
-            for ball in self.balls:
-                body = ball.body
-                body.apply_impulse((0, 500))
+            for kart in self.karts:
+                kart.body.apply_impulse((0, 30000))
